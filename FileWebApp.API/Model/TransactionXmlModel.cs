42991c60341b89c7da9c41b2a3f839a384a7cf02
Kelvin Dre <k3lvindre@gmail.com>
2021-10-15 21:15:18 +0800

    Added models.

--- FileWebApp.API/Model/TransactionXmlModel.cs
+++ FileWebApp.API/Model/TransactionXmlModel.cs
@@ -5,11 +5,16 @@ using System.Threading.Tasks;
 
 namespace FileWebApp.API.Model
 {
-    public class Transaction
+    public class TransactionXmlModel
     {
         public int TransactionId { get; set; }
         public DateTime TransactionDate { get; set; }
         public string Status { get; set; }
+        public PaymentDetails PaymentDetails { get; set; }
+    }
+
+    public class PaymentDetails
+    {
         public decimal Amount { get; set; }
         public string CurrencyCode { get; set; }
     }
