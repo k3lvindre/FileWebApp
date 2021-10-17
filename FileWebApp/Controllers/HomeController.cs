96b523d40ae3d31dbbf2bde8872d733ef02ab003
Kelvin Dre <k3lvindre@gmail.com>
2021-10-18 01:37:03 +0800

    Added file validation

--- FileWebApp/Controllers/HomeController.cs
+++ FileWebApp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace FileWebApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ITransactionService _transactionService;
-        private long mb = 1024 * 1024;
+        private readonly long megabyteConversionValue = 1024 * 1024;
 
 
         public HomeController(ILogger<HomeController> logger
@@ -34,40 +34,69 @@ namespace FileWebApp.Controllers
         {
             try
             {
-                if (uploadedFile.ContentType.Equals("application/xml") || uploadedFile.ContentType.Equals("text/xml"))
+
+                if(uploadedFile.Length > 0)
                 {
-                    if (uploadedFile.Length > 0)
+                    double fileSizeInMb = Math.Round((double)uploadedFile.Length / megabyteConversionValue, 2);
+                    
+                    if (fileSizeInMb <= 1)
                     {
-                        string tempPath = Path.GetTempFileName();
+                        string fileExtension = Path.GetExtension(uploadedFile.FileName);
 
-                        using (var stream = System.IO.File.Create(tempPath))
+                        //we can separate the convertion to single abstraction with generic type constraint
+                        //but for simplicity I intend to do all the logic here
+                        if (fileExtension == ".xml")
                         {
-                            await uploadedFile.CopyToAsync(stream);
-                        }
+                            string tempPath = Path.GetTempFileName();
 
-                        XDocument xDoc = XDocument.Load(tempPath);
-
-                        List<Transaction> transactions = xDoc.Descendants("transactions").Descendants("transaction")
-                            .Select(transaction => new Transaction
+                            using (var stream = System.IO.File.Create(tempPath))
                             {
-                                TransactionId = Convert.ToInt32(transaction.Attribute("id").Value.Replace("inv", "")),
-                                TransactionDate = Convert.ToDateTime(transaction.Element("transactiondate").Value),
-                                Status = transaction.Element("status").Value,
-                                Amount = Convert.ToDecimal(transaction.Element("paymentdetails").Element("amount").Value),
-                                CurrencyCode = transaction.Element("paymentdetails").Element("currencycode").Value
-                            }).ToList();
-                        
-                        await _transactionService.UploadTransactionsAsync(transactions);
+                                await uploadedFile.CopyToAsync(stream);
+                            }
+
+                            XDocument xDoc = XDocument.Load(tempPath);
+
+                            List<Transaction> transactions = xDoc.Descendants("transactions").Descendants("transaction")
+                                .Select(transaction => new Transaction
+                                {
+                                    TransactionId = Convert.ToInt32(transaction.Attribute("id").Value.Replace("inv", "")),
+                                    TransactionDate = Convert.ToDateTime(transaction.Element("transactiondate").Value),
+                                    Status = transaction.Element("status").Value,
+                                    Amount = Convert.ToDecimal(transaction.Element("paymentdetails").Element("amount").Value),
+                                    CurrencyCode = transaction.Element("paymentdetails").Element("currencycode").Value
+                                }).ToList();
+
+                            await _transactionService.UploadTransactionsAsync(transactions);
+                        }
+                        else if (fileExtension == ".csv")
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        return BadRequest("File size must not exceed 1 mb.");
                     }
                 }
+                else return BadRequest("File has no content.");
+
 
                 return Ok("Uploaded.");
             }
             catch (Exception ex)
             {
-                _logger.LogInformation(ex.Message);
-                return BadRequest();
+                if(ex is NullReferenceException || ex is System.Xml.XmlException)
+                {
+                    _logger.LogInformation(ex.Message);
+                    return BadRequest("XML is not in correct format. Make sure that it matches the template");
+                }
+                else 
+                { 
+                    _logger.LogInformation(ex.Message);
+                    return BadRequest();
+                }
             }
+          
         }
 
         public IActionResult Index()
